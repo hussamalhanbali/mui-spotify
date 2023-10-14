37f03b3f6320ac67ed95aeaf64d693e8711707ae
Hussam Alhambali <Alhanbali.hussam@hotmail.com>
2023-10-14 19:27:31 +0200

    Updated - App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Box, Button, Stack } from '@mui/material';
 import './App.css';
 
-function App() {
+function App({ spotifyApi }) {
+	console.log(spotifyApi);
 	return (
-		<div className="App">
+		<Box className="App">
 			<h1>Techover Self Made - Spotify</h1>
-		</div>
+			<Stack spacing={2} direction={'row'}>
+				<Button variant="text">Text</Button>
+				<Button variant="contained">Contained</Button>
+				<Button variant="outlined">Outlined</Button>
+			</Stack>
+		</Box>
 	);
 }
 
