8b26179ae7100b39590630b944e695fafd996489
Hussam Alhambali <Alhanbali.hussam@hotmail.com>
2023-10-18 20:47:57 +0200

    Fixed bug - NPlaylist

--- src/components/NavPlaylist/NavPlaylist.jsx
+++ src/components/NavPlaylist/NavPlaylist.jsx
@@ -4,18 +4,17 @@ import './NavPlaylist.css';
 
 const NavPlaylist = ({ name, id, loading }) => {
 	return (
-		<NavLink className="playlist__navlink" style={{ textDecoration: 'none' }} to={loading ? '' : `playlist/${id}`}>
+		<NavLink className="playlist__navlink" to={loading ? '' : `/playlist/${id}`} style={{ textDecoration: 'none' }}>
 			<Box
 				px={3}
 				py={1}
 				sx={{
+					color: 'text.secondary',
 					cursor: 'pointer',
-					'&:hover': { color: 'white' },
-					transition: 'color 0.2s ease-in-out',
-					fontSize: 10
+					'&:hover': { color: 'text.primary' }
 				}}
 			>
-				{loading ? <Skeleton variant="text" sx={{ fontSize: 10 }} /> : name}
+				{loading ? <Skeleton variant={'text'} height={'14px'} width={'70px'} /> : name}
 			</Box>
 		</NavLink>
 	);
