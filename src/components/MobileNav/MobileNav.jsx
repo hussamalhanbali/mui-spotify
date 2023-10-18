2408c00ff4cb24e4f4e193be820c74922ae819a1
Hussam Alhambali <Alhanbali.hussam@hotmail.com>
2023-10-18 20:49:35 +0200

    Fixed bug - MNav

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -1,17 +1,31 @@
-import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
+import { useState } from 'react';
+import { Box, BottomNavigation, BottomNavigationAction } from '@mui/material';
 import { Home, List } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 
 const MobileNav = () => {
-	const nav = useNavigate();
 	const [value, setValue] = useState(0);
+	const navigate = useNavigate();
+
+	const styledNav = { color: 'text.secondary' };
 
 	return (
-		<Box sx={{ display: { xs: 'block', md: 'none' }, color: 'text.secondary' }}>
-			<BottomNavigation sx={{ backgroundColor: 'background.paper' }} showLabels value={value} onChange={(e, value) => setValue(value)}>
-				<BottomNavigationAction label="Home" icon={<Home />} onClick={() => nav('/')} />
-				<BottomNavigationAction label="Ditt bibiliotek" icon={<List />} onClick={() => nav('/Library')} />
+		<Box sx={{ display: { xs: 'block', md: 'none' } }}>
+			<BottomNavigation
+				sx={{ bgcolor: 'background.paper' }}
+				showLabels
+				value={value}
+				onChange={(_, newValue) => {
+					setValue(newValue);
+				}}
+			>
+				<BottomNavigationAction sx={styledNav} label="Home" icon={<Home />} onClick={() => navigate('/')} />
+				<BottomNavigationAction
+					sx={styledNav}
+					label="Ditt bibliotek"
+					icon={<List />}
+					onClick={() => navigate('/library')}
+				/>
 			</BottomNavigation>
 		</Box>
 	);
