576a386827a9f2fe1d59d2b377f951fd20493c40
Hussam Alhambali <Alhanbali.hussam@hotmail.com>
2023-10-18 20:46:08 +0200

    Fixed bug - PControls

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -1,75 +1,82 @@
-import { IconButton, Slider, Stack, Typography } from '@mui/material';
-import { formatTime } from '../../utils/formatTime';
 import { PlayArrow, SkipNext, SkipPrevious, Pause } from '@mui/icons-material';
+import { IconButton, Stack, Typography, Slider, Box } from '@mui/material';
+import { formatTime } from '../../utils/formatTime';
 import { useEffect, useState } from 'react';
 
-const PlayerControls = ({ progress, is_paused, duration, player }) => {
-	const [currentProgress, setCurrentProgress] = useState(progress);
+const PlayerControls = ({ player, is_paused, duration, progress }) => {
 	const skipStyle = { width: 28, height: 28 };
-	const playStyle = { width: 38, height: 38 };
+	const [currentProgress, setCurrentProgress] = useState(progress);
 
 	useEffect(() => {
-		const intervalId = setInterval(() => {
-			if(!is_paused && player) {
-				setCurrentProgress((prevState) => prevState + 1);
+		const interval = setInterval(() => {
+			if (!is_paused && player) {
+				setCurrentProgress((c) => c + 1);
 			}
 		}, 1000);
-		return () => clearInterval(intervalId);
+		return () => clearInterval(interval);
 	}, [is_paused, player]);
 
 	useEffect(() => {
 		setCurrentProgress(progress);
 	}, [progress]);
 
+	if (!player) return <Box>Transfer playback</Box>;
+
 	return (
-		<Stack direction={'column'} spacing={2} justify="center" alignItems={'center'} sx={{ width: '100%' }}>
-			<Stack spacing={1} direction={'row'} justifyContent={'center'} alignItems={'center'} sx={{ width: '100%' }}>
+		<Stack direction="column" spacing={2} justify="center" alignItems="center" sx={{ width: '100%' }}>
+			<Stack spacing={1} direction="row" justifyContent={'center'} alignItems="center" sx={{ width: '100%' }}>
 				<IconButton
-					size="small"
-					sx={{ color: 'text.primary' }}
 					onClick={() => {
 						setCurrentProgress(0);
 						player.previousTrack();
 					}}
+					size="small"
+					sx={{ color: 'text.primary' }}
 				>
 					<SkipPrevious sx={skipStyle} />
 				</IconButton>
 				<IconButton
-					size="small"
-					sx={{ color: 'text.primary' }}
 					onClick={() => {
 						player.togglePlay();
 					}}
-				>
-					{is_paused ? <PlayArrow sx={playStyle} /> : <Pause sx={playStyle} />}
-				</IconButton>
-				<IconButton
 					size="small"
 					sx={{ color: 'text.primary' }}
+				>
+					{is_paused ? (
+						<PlayArrow sx={{ width: 38, height: 38 }} />
+					) : (
+						<Pause sx={{ width: 38, height: 38 }} />
+					)}
+				</IconButton>
+				<IconButton
 					onClick={() => {
-						setCurrentProgress(0);
 						player.nextTrack();
 					}}
+					size="small"
+					sx={{ color: 'text.primary' }}
 				>
 					<SkipNext sx={skipStyle} />
 				</IconButton>
 			</Stack>
-			<Stack spacing={2} direction="row" justifyContent={'center'} alignItems={'center'} sx={{ width: '75%' }}>
-				<Typography sx={{ color: 'text.secondary', fontSize: 12 }}>{formatTime(currentProgress)}</Typography>
+			<Stack spacing={2} direction="row" justifyContent={'center'} alignItems="center" sx={{ width: '75%' }}>
+				<Typography variant="body1" sx={{ color: 'text.secondary', fontSize: 12 }}>
+					{formatTime(currentProgress)}
+				</Typography>
 				<Slider
 					max={duration}
 					value={currentProgress}
+					onChange={(_, value) => {
+						setCurrentProgress(value);
+					}}
+					onChangeCommitted={(e, value) => {
+						player.seek(value * 1000);
+					}}
 					min={0}
 					size="medium"
-					onChange={(even, value) => {
-						console.log('changed', value);
-					}}
-					onChangeCommitted={(even, value) => {
-						player.seek(value * 1000);
-						// console.log('committed', value);
-					}}
 				/>
-				<Typography sx={{ color: 'text.secondary', fontSize: 12 }}>{formatTime(duration)}</Typography>
+				<Typography variant="body1" sx={{ color: 'text.secondary', fontSize: 12 }}>
+					{formatTime(duration)}
+				</Typography>
 			</Stack>
 		</Stack>
 	);
