d41626a45b81ac6af76a1dd2135556915fd3402c
Hussam Alhambali <Alhanbali.hussam@hotmail.com>
2023-10-18 20:48:45 +0200

    Fixed bug - NavItem

--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -1,27 +1,27 @@
-import { NavLink } from 'react-router-dom';
 import { Box } from '@mui/material';
-import './NavItem.css';
+import { Link } from 'react-router-dom';
 
-const NavItem = ({ name, Icon, target }) => {
+const NavItem = ({ name, Icon, target, active }) => {
 	return (
-		<NavLink className="NavLink" to={target} style={{ textDecoration: 'none' }}>
+		<Link to={target} style={{ textDecoration: 'none' }}>
 			<Box
 				px={3}
 				py={1}
 				sx={{
+					color: active ? 'text.primary' : 'text.secondary',
 					display: 'flex',
 					alignItems: 'center',
 					fontWeight: 'bold',
 					cursor: 'pointer',
 					'&:hover': { color: 'white' },
-					transition: 'color 0.2s ease-in-out',
+					transitions: 'color 0.2s ease-in-out',
 					fontSize: 14
 				}}
 			>
 				{Icon && <Icon sx={{ fontSize: 28, marginRight: 1 }} />}
 				{name}
 			</Box>
-		</NavLink>
+		</Link>
 	);
 };
 
