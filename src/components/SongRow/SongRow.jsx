4bbe063a7c3fa84839d3c97405c22f147d9ab935
Hussam Alhambali <Alhanbali.hussam@hotmail.com>
2023-10-18 20:41:16 +0200

    Fixed bug - SRow

--- src/components/SongRow/SongRow.jsx
+++ src/components/SongRow/SongRow.jsx
@@ -1,12 +1,22 @@
-import { Avatar, Box, Grid, Skeleton, Typography } from '@mui/material';
+import React from 'react';
+import { Avatar, Box, Typography, Grid, Skeleton } from '@mui/material';
 import { formatTime } from '../../utils/formatTime';
 
-const SongRow = ({ images, artist, album, title, duration, i, loading, position, spotifyApi, contextUri }) => {
-	const image = images?.length > 0 ? images[0].url : null;
+const SongRow = ({ images, title, artist, album, duration, i, loading, spotifyApi, contextUri, position }) => {
+	const image = images?.length > 0 ? images[0] : null;
 
 	const onRowClick = async () => {
-		const song = { context_uri: contextUri, offset: { position: position }, position_ms: 0 };
-		await spotifyApi.play();
+		const song = {
+			context_uri: contextUri,
+			offset: { position },
+			position_ms: 0,
+			title,
+			image: image ? image : {},
+			artist,
+			duration,
+			position
+		};
+		await spotifyApi.play(song);
 	};
 
 	return (
@@ -14,40 +24,53 @@ const SongRow = ({ images, artist, album, title, duration, i, loading, position,
 			onClick={onRowClick}
 			container
 			px={2}
-			p={1}
+			py={1}
 			sx={{
 				width: '100%',
 				color: 'text.secondary',
 				fontSize: 14,
 				cursor: 'pointer',
-				'&:hover': { backgroundColor: '#ffffff10' }
+				'&:hover': { bgcolor: '#ffffff10' }
 			}}
 		>
-			<Grid sx={{ width: 35, display: 'flex', alignItems: 'center', fontSize: 16 }} item>
-				{/* position */}
+			<Grid item sx={{ width: 35, display: 'flex', alignItems: 'center', fontSize: 16 }}>
 				{i + 1}
 			</Grid>
-			<Grid sx={{ flex: 1, display: 'flex', alignItems: 'center', gap: 2 }} item>
-				{/* Title */}
+			<Grid item sx={{ flex: 1, display: 'flex', alignItems: 'center', gap: 2 }}>
 				{loading ? (
 					<Skeleton variant="rectangular" width={40} height={40} />
 				) : (
-					<Avatar src={image} alt={null} variant="square" />
+					<Avatar src={image?.url} alt={title} variant="square" />
 				)}
-				<Box>
+				<Box ml={1}>
 					<Typography sx={{ fontSize: 16, color: 'text.primary' }}>
 						{loading ? <Skeleton variant="text" width={130} height={24} /> : title}
 					</Typography>
-					<Typography sx={{ fontSize: 14, color: 'text.secondary' }}>
+					<Typography sx={{ fontSize: 12, color: 'text.secondary' }}>
 						{loading ? <Skeleton variant="text" width={50} height={18} /> : artist}
 					</Typography>
 				</Box>
 			</Grid>
-			<Grid xs={3} sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }} item>
-				{loading ? <Skeleton variant="text" width={50} height={24} /> : album}
+			<Grid
+				item
+				xs={3}
+				sx={{
+					display: { xs: 'none', md: 'flex' },
+					alignItems: 'center'
+				}}
+			>
+				{loading ? <Skeleton variant="text" width={50} height={14} /> : album}
 			</Grid>
-			<Grid xs={3} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }} item>
-				{loading ? <Skeleton variant="text" width={50} height={24} /> : formatTime(duration)}
+			<Grid
+				item
+				xs={3}
+				sx={{
+					display: 'flex',
+					justifyContent: 'flex-end',
+					alignItems: 'center'
+				}}
+			>
+				{loading ? <Skeleton variant="text" width={50} height={14} /> : formatTime(duration)}
 			</Grid>
 		</Grid>
 	);
